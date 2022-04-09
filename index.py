7e26ffb10653d52d2991f2e978fa34abefc59a3b
sadegh-dev <sadegh.rez@gmail.com>
2022-04-09 15:50:10 +0430

    set class variable

--- index.py
+++ index.py
@@ -1,2 +1,15 @@
+
 class Person :
-    pass
+    counter = 0
+    def __init__(self):
+        Person.counter += 1
+        print(f'Person number {Person.counter}')
+
+p1 = Person()
+p2 = Person()
+p3 = Person()
+p4 = Person()
+
+
+
+print(Person.__dict__['counter'])
