731b03eb1f878ec1b523b68a4f0e2ce8df1b9679
sadegh-dev <sadegh.rez@gmail.com>
2022-04-10 13:32:27 +0430

    write __init__ for User and Person

--- inheritance.py
+++ inheritance.py
@@ -1,11 +1,14 @@
 class Person:
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
+        print(f'Create Person by name is {name}')
 
 
 class User(Person):
-    def __init__(self):
-        pass
+    def __init__(self, email, name):
+        super().__init__(name)
+        self.username = email
+        print(f'Create User by username is {email}')
 
 
 
